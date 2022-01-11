743d5c32dcc9fa75ce1b83874219a9cdeb9c15d5
Ethan Isola <isolaethan@gmail.com>
2022-01-11 00:24:49 -0600

    commit tree 8522ad165eeca041dfa33768632457a02b5b2fad

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+saturdayFun();
+function saturdayFun(fun = 'roller-skate'){
+    return `This Saturday, I want to ${fun}!`
+}
+
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(wrap){
+    const part1 = "You are";
+    return function(adj){
+        return `${part1} ${wrap}${adj}${wrap}!`;
+    }
+  }
